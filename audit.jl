8b3271199e71ea63b393be127bfe776a890631c8
Kevin Bonham, PhD <kbonham@wellesley.edu>
2024-03-20 18:57:14 -0400

    update airtable

--- audit.jl
+++ audit.jl
@@ -1,3 +1,4 @@
+using VKCComputing: audit_update_remote!
 # # VKC Lab sequencing files audit
 #
 # Checking to make sure we have all expected files,
@@ -33,7 +34,47 @@ sps.keep = Bool.(sps.keep)
 aws_files = aws_ls("s3://wc-vanja-klepac-ceraj/sequencing/processed/mgx/"; profile="wellesley")
 local_files = get_analysis_files()
 
+# get files that didn't match pattern (they mostly don't have S_wells)
 aws_nosp = subset(aws_files, "seqprep"=>ByRow(ismissing)).file
+
+delete_aws = let
+	del = String[]
+	# Get the problematic seqids from the file names
+	seqs = Set(filter(startswith("SEQ"), first.(split.(aws_nosp, r"[\._]"))))
+	# make a new df that has filename-derrived seqids
+	df = transform(aws_files, "file"=>  ByRow(f-> first(split(f, r"[\._]"))) => "seqprep_from_file")
+	# take only the ones that are problematic
+	subset!(df, "seqprep_from_file" => ByRow(s-> s ∈ seqs))
+	gdf = groupby(df, "seqprep_from_file")
+	for g in gdf
+		# make sure there is exactly one non-missing S_well in the subdf
+		swell = only(unique(filter(!ismissing, g.S_well)))
+		# ensure that the correct file is uploaded
+		@assert all(eachrow(g)) do row
+			# don't care about files that were correctly parsed
+			!ismissing(row.seqprep) && return true
+			# there are some random files leftover from kneaddata that we want to delete
+			contains(row.file, "joined.fastq.gz") && return true
+			id = row.seqprep_from_file
+			filesize = row.size
+			# this is what the name should be
+			newname = replace(row.file, id=> "$(id)_$swell")
+			subdf = subset(g, "file"=> ByRow(==(newname)))
+			 
+			# make sure the the name actually got changed, and the new name already exists
+			size(subdf, 1) == 1 && newname != row.file && only(subdf.size) == filesize  
+		end
+		append!(del, subset(g, "seqprep"=> ByRow(ismissing)).path)
+	end
+	del
+end
+
+for file in delete_aws[6:end]
+	run(`aws s3 rm $file --profile wellesley`)
+end
+
+
+
 local_nosp = subset(local_files, "seqprep"=>ByRow(ismissing)).file
 
 let seqs = filter(startswith("SEQ"), local_nosp)
@@ -50,3 +91,20 @@ let seqs = filter(startswith("SEQ"), local_nosp)
 		end
 	end
 end
+
+(airtable_seq, local_seq, good_files, problem_files) = audit_analysis_files(local_files; base)
+
+@info "Ambiguous S_well: $(count(problem_files.s_well_ambiguity))"
+@info "Bad suffix: $(count(problem_files.bad_suffix))"
+@info "Bad UID: $(count(problem_files.bad_uid))"
+
+VKCComputing.audit_update_remote!(
+	select(airtable_seq, "S_well"=>"s_well", Cols(:)),
+	select(local_seq, "S_well"=>"s_well", Cols(:));
+	dryrun=true
+)
+
+aws_files.source .= "aws"
+local_files.source .= "grace"
+
+
